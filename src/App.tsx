4f342bff18f9f7f3ca2c5fe7b0ed48af7d60348f
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-05-20 16:16:01 +0530

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
 
-
   return (
-    <Button colorPalette="blue">Click Me</Button>
-  )
+
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" hideBelow="lg">
+        aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
