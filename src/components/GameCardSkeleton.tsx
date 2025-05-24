2e77fa382223b6e13eecff5580891d3511ca6ff1
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-05-24 19:59:21 +0530

    Refactor: remove duplicate styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import { Card, Skeleton, SkeletonText } from "@chakra-ui/react";
 function GameCardSkeleton() {
   
   return (
-    <Card.Root width={'300px'} borderRadius={10} overflow={'hidden'}>
+    <Card.Root>
       <Skeleton height={'220px'} />
       <Card.Body>
         <SkeletonText />
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,25 +2,36 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
-
-
+import GameCardContainer from "./GameCardContainer";
 
 function GameGrid() {
-
-  const {games, error, isLoading} = useGames();
-  const skeleton = [1,2,3,4,5,6,7,8];
+  const { games, error, isLoading } = useGames();
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} gap={10} padding='10px'>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        gap={10}
+        padding="10px"
+      >
         {/* Skeleton Loading */}
-        {isLoading && skeleton.map(sk => <GameCardSkeleton key={sk} />)}
+        {isLoading &&
+          skeleton.map((sk) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={sk} />
+            </GameCardContainer>
+          ))}
         {/* Skeleton Loading */}
-        {games.map(g => <GameCard key={g.id} game={g} />)}
+        {games.map((g) => (
+          <GameCardContainer>
+            <GameCard key={g.id} game={g} />
+          </GameCardContainer> 
+        ))}
       </SimpleGrid>
     </>
-  )
+  );
 }
 
 export default GameGrid;
