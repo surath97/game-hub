cfc019006fdad98923a7103b3a825950aa681406
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-07-02 18:18:13 +0530

    Install and setup tanstack query library

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ThemeProvider } from 'next-themes' 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import App from './App.tsx'
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
       <ThemeProvider attribute="class" disableTransitionOnChange={false} defaultTheme='dark'>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </ThemeProvider>
     </ChakraProvider>
   </StrictMode>,
